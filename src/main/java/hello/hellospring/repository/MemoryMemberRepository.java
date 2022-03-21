e44152fc8bebb7db48c257fd6032cc4f82f64399
superson99 <thseogns98@naver.com>
2022-03-21 23:28:06 +0900

    20220321

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -29,4 +29,7 @@ public class MemoryMemberRepository implements MemberRepository{
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
     }
+    public void clearStore(){
+        store.clear();
+    }
 }
